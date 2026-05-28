e14a6975eabd8e239405473359e05f81c90bb8e4
semper44 <emmanuelcopeters@gmail.com>
2026-05-28 02:59:40 +0100

    configuring react

--- backend/django_python/urls.py
+++ backend/django_python/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard_view, DummyApp, receive_ci_results
+from .views import dashboard_view, DummyApp, CreateUserSelectedRepos, receive_ci_results
 
 
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", dashboard_view),
     path("results/", receive_ci_results, name="results"),
     path("list/", DummyApp.as_view(), name="list_users"),
+    path('workspaces/repositories/create', CreateUserSelectedRepos.as_view(), name='repo-bulk-create'),
+
 ]
