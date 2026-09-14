9f48d67224d2537443372a4e910757a5edcc019c
semper44 <emmanuelcopeters@gmail.com>
2026-09-14 18:58:32 +0100

    sorted allowed host

--- backend/odozi/settings.py
+++ backend/odozi/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     "fast-results-behave.loca.lt",
-    "short-aliens-sniff.loca.lt"
+    "short-aliens-sniff.loca.lt",
     "odozi.onrender.com"
 ]
 
