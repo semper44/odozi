2ffb5ca9ce8b45dcac8626f6b0fe1d782c5f1975
semper44 <emmanuelcopeters@gmail.com>
2026-09-14 11:56:33 +0100

    solved app registry issue

--- backend/odozi/asgi.py
+++ backend/odozi/asgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
+import django
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odozi.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
